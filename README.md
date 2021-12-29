d2214ae3297e8bd9d3529f8964e22516bae33d45
rafael cardoso <rafael.monteiro@semantix.com.br>
2021-12-29 13:51:36 -0300

    chore: adding some more features

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# This is our first SDP library for trino.
+# This is our first library for trino.
 
+First of all, this library is made based on [Trino library](https://github.com/trinodb/trino-python-client).
 
+# Goal
+
+At first this project will help users to make easy connections with Trino and presenting some features that will guide them throgh the Datalake allowing them to get:
+- Catalogs
+- Schemas
+- Tables
 
 # For the testing you need to install 
 
@@ -9,3 +16,5 @@ pip install pytest==4.4.1
 
 - python setup.py pytest
 - [Resources](https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f)
+
+
--- trinoconnectorlib/myfunctions.py
+++ trinoconnectorlib/myfunctions.py
@@ -1,23 +1,54 @@
 import trino
+from getpass import getpass
 
-def datalake_connection(auth, trino_username, trino_catalog, sdp_schema, trino_password):
-    conn = trino.dbapi.connect(
-        host='localhost',
-        port=8080,
-        user=trino_username,
-        catalog=trino_catalog,
-        schema=sdp_schema,
-    )
+# Opening connection with Datalake
+def datalake_connection(auth, trino_username, trino_catalog, trino_schema, password=None):
+    password = getpass()
+    print(password)
+
+    if password != '': 
+        conn = trino.dbapi.connect(
+            host='localhost',
+            port=8080,
+            user=trino_username,
+            catalog=trino_catalog,
+            schema=trino_schema,
+            http_scheme='https',
+            auth=trino.auth.BasicAuthentication(trino_username, password),
+        )
+    else:
+        conn = trino.dbapi.connect(
+            host='localhost',
+            port=8080,
+            user=trino_username,
+            catalog=trino_catalog,
+            schema=trino_schema,
+        )
+
     cur = conn.cursor()
     return cur
 
-def datalake_connection_user(auth=False, trino_username=None, trino_catalog=None, sdp_schema=None, trino_password=None):
-    cur = datalake_connection(auth, trino_username, trino_catalog, sdp_schema, trino_password)
-    cur.execute('SELECT * FROM system.runtime.nodes')
+# Get all the catalogs available.
+def datalake_get_catalogs(auth, trino_username, trino_catalog, trino_schema):
+    cur = datalake_connection(auth, trino_username, trino_catalog, trino_schema)
+    cur.execute(f'SHOW catalogs')
+    rows = cur.fetchall()
+    return rows
+
+# Get Schemas from a specific catalog.
+def datalake_get_schemas(auth, trino_username, trino_catalog, trino_schema):
+    cur = datalake_connection(auth, trino_username, trino_catalog, trino_schema)
+    cur.execute(f'SHOW SCHEMAS FROM {trino_catalog}')
+    rows = cur.fetchall()
+    return rows
+
+# Get all tables from a schema.
+def datalake_get_tables(auth, trino_username, trino_catalog, trino_schema):
+    cur = datalake_connection(auth, trino_username, trino_catalog, trino_schema)
+    cur.execute(f'SHOW TABLES FROM {trino_catalog}.{trino_schema}')
     rows = cur.fetchall()
     return rows
 
 
 if __name__ == '__main__':
-    rows = datalake_connection_user(True, "admin", "system", "runtime")
-    print(rows)
+    rows = datalake_get_catalogs(auth=False, trino_username='admin', trino_catalog='system', trino_schema='runtime')
